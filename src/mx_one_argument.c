ecf34cf4943f14893b387d82ba89734ef607772b
Vladyslav Struk <vstruk@e2r7p5.unit.ua>
2020-02-01 16:00:56 +0200

    charg character special

--- src/mx_one_argument.c
+++ src/mx_one_argument.c
@@ -5,7 +5,7 @@ void mx_one_argument(t_data *data) {
         if (data->flags[4] || data->flags[5] || data->flags[3])
             mx_print_l(data->cnst, data);
         else {
-            if (data->flags[8] && data->size > 1) {
+            if (data->flags[8]) {
                 mx_printstr_update("total ",
                 data->strtotal, "\n", NULL, NULL);
             }
--- src/mx_other_flags.c
+++ src/mx_other_flags.c
@@ -10,7 +10,7 @@ void mx_other_flags(char *cont, t_data *app) {
     if (mx_memchr(cont, 'a', len))
         app->flags[0] = 1;
     if(mx_memchr(cont, 'f', len))
-        app->flags[9] = 1;       // спросить у Влада по-поводу флага -a (он связан с флагом -f)
+        app->flags[9] = 1;
     else if (mx_memchr(cont, 'A', len))
         app->flags[1] = 1;
 }
